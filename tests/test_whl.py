4c3e5c744ae0936293aeee251350de25f1fd0049
Leo <zeng.yuchen@hkaco.com>
2025-08-26 14:06:30 +0800

    test: add package info checking

---
+++ tests/test_whl.py
@@ -0,0 +1,24 @@
+from importlib.metadata import metadata
+
+
+def test_skydel_metadata():
+    pkg_metadata = metadata("skydel")
+
+    print("====== Package Info ======")
+    print(pkg_metadata)
+
+
+def test_skydel_import():
+    print("====== Package Import ======")
+    import skydel
+
+    print("Current Package Version", skydel.__version__)
+
+    from skydel.skydelsdx import commands
+
+    print(f"Current API Version: {commands.ApiVersion}")
+
+
+if __name__ == "__main__":
+    test_skydel_metadata()
+    test_skydel_import()
